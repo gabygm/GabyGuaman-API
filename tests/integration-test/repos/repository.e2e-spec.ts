d927af51b6c47884cfcb592516db310f4ee137ab
Gaby <paogaby0803@gmail.com>
2023-04-22 13:53:49 -0500

    test: adds e2e test for mappers for api service

---
+++ tests/integration-test/repos/repository.e2e-spec.ts
@@ -0,0 +1,28 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { INestApplication } from '@nestjs/common';
+import * as request from 'supertest';
+import { AppModule } from '../../../src/app.module';
+import { repositoriesDTOMock } from '../../mocks';
+import { RepositoryService } from '../../../src/repository/repository.service';
+import { PrismaService } from '../../../src/prisma.service';
+
+describe('AppController (e2e)', () => {
+  let app: INestApplication;
+
+  beforeEach(async () => {
+    const moduleFixture: TestingModule = await Test.createTestingModule({
+      imports: [AppModule],
+      providers: [RepositoryService, PrismaService],
+    }).compile();
+
+    app = moduleFixture.createNestApplication();
+    await app.init();
+  });
+
+  test('/repository (GET) Should return 200 when there is data', () => {
+    return request(app.getHttpServer())
+      .get('/repository')
+      .expect(200)
+      .expect(repositoriesDTOMock);
+  });
+});
