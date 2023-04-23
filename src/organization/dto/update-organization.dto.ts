3bfd5fb16ad402caf18a04a29ca9f0cf82ec7baf
Gaby <paogaby0803@gmail.com>
2023-04-22 22:04:42 -0500

    feat: creates updateDTO for organization

--- src/organization/dto/update-organization.dto.ts
+++ src/organization/dto/update-organization.dto.ts
@@ -1,19 +1,18 @@
-import { Organization } from "@prisma/client";
+import { CreateOrganizationDto } from './create-organization.dto';
 import { IsNumber, IsOptional, IsString } from 'class-validator';
+import { Organization } from '@prisma/client';
 
-
-export class CreateOrganizationDto {
+export class UpdateOrganizationDto {
     @IsOptional()
-    @IsNumber()  
-    id: number
     @IsString()
     name: string
+    @IsOptional()
     @IsNumber()             
     status: number
+
 
     fromEnttity(orgnanization: Organization) {
         this.name = orgnanization.name, 
         this.status = orgnanization.status
       }
 }
-
