dc2268fed2dd4ca40ef990c6e007b3e6ac600124
Gaby <paogaby0803@gmail.com>
2023-04-23 08:45:18 -0500

    feat: create dto for endpoint repo merics

---
+++ src/repository/dto/metrics-repository.dto.ts
@@ -0,0 +1,49 @@
+import { Repository, Organization } from '@prisma/client';
+import { IsNumber, IsString } from 'class-validator';
+
+
+class MetricsRepositoryDto {
+    @IsNumber()  
+    id: bigint
+    @IsString()
+    name: string
+    @IsString()
+    tribe: string
+    @IsString()
+    organization: string
+    @IsString()
+    coverage: string
+    @IsNumber()
+    codeSmells: string
+    @IsNumber()
+    bugs: string
+    @IsNumber()
+    vulnerabilities: string
+    @IsString()
+    hotspots: string
+    @IsString()
+    verificationState: string
+    @IsString()
+    state: string
+}
+export class ListMetricsRepositoryDto {
+    repositories: MetricsRepositoryDto[] = []
+
+     mapToDTO(repos) {
+        repos.forEach((repo)=>{
+            let metricsRepositoryDto: MetricsRepositoryDto = new MetricsRepositoryDto()
+            metricsRepositoryDto.id =repo.id.toString()
+            metricsRepositoryDto.name = repo.name
+            metricsRepositoryDto.tribe = repo['tribu']?.name
+            metricsRepositoryDto.organization = repo['tribu']?.organization.name
+            metricsRepositoryDto.coverage = repo['metrics'][0]?.coverage
+            metricsRepositoryDto.codeSmells = repo['metrics'][0]?.code_smells
+            metricsRepositoryDto.bugs = repo['metrics'][0]?.bugs
+            metricsRepositoryDto.vulnerabilities = repo['metrics'][0]?.vulnerabilities
+            metricsRepositoryDto.hotspots = repo['metrics'][0]?.hotspot
+            metricsRepositoryDto.verificationState = "test"
+            metricsRepositoryDto.state = repo.state
+            this.repositories.push(metricsRepositoryDto)
+        })
+    }
+}
